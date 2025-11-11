5be0a3cea3a747de4cd68eb3e604aad798ebc597
Wilson Ler <lws803@gmail.com>
2025-11-11 16:08:42 -0500

    added basic layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Serif, IBM_Plex_Mono } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ibmPlexSerif = IBM_Plex_Serif({
+  variable: "--font-ibm-plex-serif",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ibmPlexMono = IBM_Plex_Mono({
+  variable: "--font-ibm-plex-mono",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -25,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ibmPlexSerif.variable} ${ibmPlexMono.variable} antialiased`}
       >
         {children}
       </body>
---
+++ components/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+import JericaIcon from "./icons/jerica";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between w-full py-4 px-8">
+      <Link href="/">
+        <JericaIcon className="w-10 h-10" />
+      </Link>
+      <div className="flex items-center gap-4">
+        <Link href="/">about</Link>
+        <Link href="/">blogs</Link>
+        <Link href="/">values</Link>
+        <Link href="/">case studies</Link>
+        <Link href="/">say hi!</Link>
+      </div>
+    </nav>
+  );
+}
